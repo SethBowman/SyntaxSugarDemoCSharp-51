4ccea33523473b2e92080a6bb3d522c3ccedfce7
Seth Bowman <brandonsethbowman@gmail.com>
2024-10-17 16:31:30 -0500

    demo completed

---
+++ SyntaxSugarDemoCSharp-51/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SyntaxSugarDemoCSharp_51;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Explicit typing
+        int number = 10;
+        string word;
+        
+        //Inferred typing
+        var myVariable = true;
+        
+        //Inline if (ternary operator)
+        var age = 3;
+        
+        Console.WriteLine(age >= 18 ? "You're an adult" : age < 5 ? "You're a child" : "You're a minor");
+        
+        //String interpolation vs concatenation
+        var middleInitial = 'S';
+
+        Console.WriteLine($"Brandon {middleInitial} Bowman");
+        Console.WriteLine("Brandon" + " " + middleInitial + " " + "Bowman");
+
+    }
+}
